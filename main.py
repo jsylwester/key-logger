8fa66e717aba18463b615b7b91c1bbedec8ced28
jsylwester <jsylwester@tudelft.nl>
2025-11-19 15:04:14 +0100

    Added writing key-logs to files

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 import time
+import datetime
 from pynput import keyboard
+
+current_session = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S.%f")
+
+#Writing to file
+session_file = open(f"key-logs/session-{current_session}.txt", "a")
+
 
 def on_key_press(key):
     print("Key pressed: ", key)
+    session_file.write(str(key))
     if hasattr(key, "char") and key.char == "z":
         print("Z PRESSED!")
+        session_file.write('Z')
+
 
 def on_key_release(key):
-    print("Key released: ", key)
     if key == keyboard.Key.shift:
         print("SHIFT KEY RELEASED!")
+        session_file.write(f" Key released:SHIFT ")
 
 keyboard_listener = keyboard.Listener(
     on_press=on_key_press,
     on_release=on_key_release
 )
 
+
+
 print("Starting the keyboard listener, will run for 5 seconds..")
 keyboard_listener.start()
 
-time.sleep(30)
+time.sleep(10)
 print("Time's up, stopping the keyboard listener")
 
 keyboard_listener.stop()
 keyboard_listener.join()
+session_file.close()
